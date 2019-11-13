a1727486dbdea0ab3145ed01accb6232956bd38b
Gaël Aglin <aglingael@gmail.com>
2019-11-13 11:06:20 +0100

    Correct travis and config appveyor and tox

--- python_library/dl85/classifiers/tests/test_ODTClassifier.py
+++ python_library/dl85/classifiers/tests/test_ODTClassifier.py
@@ -23,15 +23,19 @@ def test_predict():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
     clf1 = ODTClassifier(max_depth=randrange(1, 4), min_sup=randrange(1, X_train.shape[0] // 4))
     clf1.fit(X_train, y_train)
-    y_pred1 = clf1.predict(X_test)
 
-    def is_class(y_pred, classes):
-        for i in y_pred:
-            if i not in list(set(list(clf1.classes_))):
-                return False
-        return True
+    if clf1.sol_size_ in [8, 9]:
+        y_pred1 = clf1.predict(X_test)
 
-    assert clf1.sol_size_ in [4, 5] or (len(y_pred1) == X_test.shape[0] and is_class(y_pred1, clf1.classes_) is True)  # list(set(y_pred1)) == list(set(list(clf1.classes_)))
+        def is_class(y_pred):
+            for i in y_pred:
+                if i not in list(set(list(clf1.classes_))):
+                    return False
+            return True
+
+        assert len(y_pred1) == X_test.shape[0] and is_class(y_pred1) is True  # list(set(y_pred1)) == list(set(list(clf1.classes_)))
+    else:
+        assert clf1.sol_size_ in [4, 5]
 
 
 check_estimator(ODTClassifier)
