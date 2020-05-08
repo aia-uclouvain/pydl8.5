c2f01e0f2d14e1dd6d7f07ccc712e7548dd3a6a8
Siegfried Nijssen <siegfried.nijssen@cs.kuleuven.be>
2020-05-08 19:21:16 +0200

    Modified the examples, such that they reflect the IJCAI demo paper.

--- python_library/examples/plot_predictor_user.py
+++ python_library/examples/plot_predictor_user.py
@@ -2,7 +2,24 @@
 =============================================
 DL8.5 Predictor used for task involving label
 =============================================
+PyDL8.5 allows users to write their own error function. This example shows how 
+to write an error function based on transaction identifiers. PyDL8.5 will determine
+these transaction identifiers (tids) based on the occurrences of an itemset in the
+training data. 
 
+The error function is called very often, and calculating an error score based
+on tids can be time consuming. For classification tasks, it is highly recommended
+not to write an error function in Python that operators on lists of tids. 
+check the plot_classifier_user_1.py example for a more efficient user-written
+error function in classification settings.
+
+Moreover, this example shows how to use the DL85Predictor class. Using this
+class, the labels do not need to be passed to DL8.5. For classification tasks,
+the error function can also be specififed as a parameter of the DL85Classifier
+class. In this case, a standard implementation is used for filling in the
+class labels for leafs in the tree. 
+
+Another example of a user-specified error function is given in plot_cluster_user.py.
 """
 import numpy as np
 from sklearn.metrics import confusion_matrix
@@ -26,20 +43,16 @@ print("#########################################################################
 
 
 # return the error and the majority class
-def error(tids, y):
-    classes, supports = np.unique(y.take(list(tids)), return_counts=True)
+def error(tids):
+    classes, supports = np.unique(y_train.take(list(tids)), return_counts=True)
     maxindex = np.argmax(supports)
     return sum(supports) - supports[maxindex], classes[maxindex]
 
 
-clf = DL85Predictor(max_depth=2, error_function=lambda tids: error(tids, y_train), time_limit=600)
+clf = DL85Predictor(max_depth=2, error_function=error, time_limit=600)
 start = time.perf_counter()
 print("Model building...")
-clf.fit(X_train, y_train)
+clf.fit(X_train)
 duration = time.perf_counter() - start
 print("Model built. Duration of building =", round(duration, 4))
-y_pred = clf.predict(X_test)
-print("Confusion Matrix below")
-print(confusion_matrix(y_test, y_pred))
 print("Accuracy DL8.5 on training set =", round(clf.accuracy_, 4))
-print("Accuracy DL8.5 on test set =", round(accuracy_score(y_test, y_pred), 4))
