734ddeb84b0c61041563e27c97e43b71c9b97686
Valentin Lemaire <v.lemaire@student.uclouvain.be>
2022-04-08 13:17:05 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import Extension, find_packages, setup
 from Cython.Build import cythonize
 import platform
 import codecs
-from dl85 import __version__
+from dl85._version import __version__
 
 
 DISTNAME = 'pydl8.5'
