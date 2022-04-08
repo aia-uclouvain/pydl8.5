e8fd564f8898f7e19b3a2cb2cd990d19ee23d56e
Valentin Lemaire <v.lemaire@student.uclouvain.be>
2022-04-08 13:25:22 +0200

    changed _version file to avoid import error

--- dl85/__init__.py
+++ dl85/__init__.py
@@ -5,6 +5,8 @@ from .supervised.classifiers.boosting import DL85Booster, MODEL_LP_RATSCH, MODEL
 from .predictors.predictor import DL85Predictor
 from .predictors.distribution_predictor import DL85DistributionPredictor
 from .unsupervised.clustering import DL85Cluster
-from ._version import __version__
+# from .._version import __version__
 
-__all__ = ['__version__', 'DL85Predictor', 'DL85DistributionPredictor', 'DL85Classifier', 'DL85Booster', 'DL85Cluster', 'DL85Regressor', 'DL85DistributionRegressor']
+# __all__ = ['__version__', 'DL85Predictor', 'DL85DistributionPredictor', 'DL85Classifier', 'DL85Booster', 'DL85Cluster', 'DL85Regressor', 'DL85DistributionRegressor']
+
+__all__ = ['DL85Predictor', 'DL85DistributionPredictor', 'DL85Classifier', 'DL85Booster', 'DL85Cluster', 'DL85Regressor', 'DL85DistributionRegressor']
